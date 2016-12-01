b76e92701db0598b7914943cd8f98a7b7508fc69
Serge van den Oever <serge.van.den.oever@macaw.nl>
2016-12-01 22:19:02 +0100

    fixed differences proxy/real, updated sp-rest-proxy, added npm run commands

--- ShowTitleRestWithProxy/deploy.js
+++ ShowTitleRestWithProxy/deploy.js
@@ -1,22 +1,32 @@
+var Cpass = require("cpass");
+var cpass = new Cpass();
 var spsave = require("spsave").spsave;
 var fs = require('fs');
+var configFile = __dirname + '/config/_private.conf.json';
+
+if (!fs.existsSync(configFile)) {
+    console.log('Missing configuration "' + configFile + '". Run "npm run serve" and save configuration settings');
+    process.exit(0);
+}
+
+var config = require(configFile);
 
 var coreOptions = {
-    siteUrl: process.env.SPONLINE_SITE_APPS,
+    siteUrl: config.siteUrl,
     notification: true,
     checkin: true,
     checkinType: 1
 };
 var creds = {
-    username: process.env.SPONLINE_USERNAME,
-    password: process.env.SPONLINE_PASSWORD,
+    username: config.username,
+    password: cpass.decode(config.password),
     domain: '' // [domain (on required for on premise)]
 };
  
 var fileOptions = {
     folder: 'apppages/showtitlerest',
     fileName: 'index.aspx',
-    fileContent: fs.readFileSync('index.aspx')
+    fileContent: String(fs.readFileSync('static/index.html')).replace('http://localhost:8081', config.siteUrl)
 };
 
 spsave(coreOptions, creds, fileOptions)
--- ShowTitleRestWithProxy/server.js
+++ ShowTitleRestWithProxy/server.js
@@ -2,7 +2,7 @@ var RestProxy = require("sp-rest-proxy");
  
 var settings = {
     configPath: __dirname + "/config/_private.conf.json", // Location for SharePoint instance mapping and credentials 
-    port: 8080,                                           // Local server port 
+    port: 8081,                                           // Local server port 
     staticRoot: __dirname + "/static"                     // Root folder for static content 
 };
  
